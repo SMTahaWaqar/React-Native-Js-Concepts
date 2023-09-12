1faad302877bc9ead21ef48bceaec9af04f47acd
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-09-12 16:17:06 +0500

    section list

--- App.js
+++ App.js
@@ -1,39 +1,43 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {FlatList, SectionList, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   const data = [
     {
       id: 1,
       name: 'Taha',
+      data: ['Js', 'Python', 'Java'],
     },
     {
       id: 2,
       name: 'Umer',
+      data: ['Js', 'Python', 'Java'],
     },
     {
       id: 3,
       name: 'Arham',
+      data: ['Js', 'Python', 'Java'],
     },
     {
       id: 4,
       name: 'Maaz',
+      data: ['Js', 'Python', 'Java'],
     },
     {
       id: 5,
       name: 'Ali',
+      data: ['Js', 'Python', 'Java'],
     },
   ];
   return (
     <View>
-      <Text style={{fontSize: 30}}>Component in FlatList</Text>
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <View>
-            <Text>{item.id}</Text>
-            <Text>{item.name}</Text>
-          </View>
+      <Text style={{fontSize: 30}}>Section List in React Native</Text>
+      <SectionList
+        sections={data}
+        renderItem={({item}) => <Text style={{fontSize: 25}}>{item}</Text>}
+        renderSectionHeader={({section: {name}}) => (
+          <Text style={{fontSize: 25, color: 'red'}}>{name}</Text>
         )}
       />
     </View>
