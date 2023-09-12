0b4d4bab6b08b8e9099d946b23926c1acfe9728c
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-09-12 13:33:50 +0500

    button on press

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
-import UniversityData from './components/UniversityData';
 
 const App = () => {
-  return (
-    <View>
-      <UniversityData />
-      <Text style={{fontSize: 30}}>Components</Text>
-      <UserData />
-    </View>
-  );
-};
+  const fruit = () => {
+    console.warn('Fucntion called');
+  };
 
-const UserData = () => {
   return (
     <View>
-      <Text style={{fontSize: 15}}>Name : Taha</Text>
-      <Text style={{fontSize: 15}}>Age : 21</Text>
-      <Text style={{fontSize: 15}}>Email : taha@developer.com</Text>
+      <Text style={{fontSize: 30}}>Button And OnPress Event</Text>
+      <Button title="On Press" onPress={fruit} color={'black'} />
     </View>
   );
 };
