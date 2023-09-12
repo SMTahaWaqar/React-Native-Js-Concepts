7ff36ab067043e3065dc68130bfe3e34894f1f3a
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-09-12 13:59:05 +0500

    basic form

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
   const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [display, setDisplay] = useState(false);
+  const resetFormData = () => {
+    setDisplay(false);
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
   return (
     <View>
-      <Text style={{fontSize: 30}}>Handle Text Input</Text>
-      <Text style={{fontSize: 30}}>Name: {name}</Text>
+      <Text style={{fontSize: 30}}>Simple Form In React Native</Text>
       <TextInput
-        style={styles.textInput}
         placeholder="Enter your name"
+        value={name}
+        style={styles.textInput}
         onChangeText={text => setName(text)}
       />
+      <TextInput
+        placeholder="Enter your email"
+        value={email}
+        style={styles.textInput}
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Enter a password"
+        value={password}
+        secureTextEntry={true}
+        style={styles.textInput}
+        onChangeText={text => setPassword(text)}
+      />
+      <View style={{marginBottom: 10}}>
+        <Button
+          color={'green'}
+          title="Print Details"
+          onPress={() => setDisplay(true)}
+        />
+      </View>
+      <Button title="Clear Details" onPress={resetFormData} />
+      {display ? (
+        <View>
+          <Text style={{fontSize: 15}}>User Name is : {name}</Text>
+          <Text style={{fontSize: 15}}>Email : {email}</Text>
+          <Text style={{fontSize: 15}}>Password : {password}</Text>
+        </View>
+      ) : null}
     </View>
   );
 };
@@ -19,7 +56,7 @@ const App = () => {
 const styles = StyleSheet.create({
   textInput: {
     fontSize: 18,
-    color: 'red',
+    color: 'blue',
     borderWidth: 2,
     borderColor: 'blue',
     margin: 10,
