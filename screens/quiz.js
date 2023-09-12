c24588c0cc415191a7d3e4632b62195c3fafd90a
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-09-12 13:05:38 +0500

    View, Text, Button

---
+++ screens/quiz.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Quiz = () => {
+  return (
+    <View>
+      <Text>This is Quiz</Text>
+    </View>
+  );
+};
+
+export default Quiz;
+
+const styles = StyleSheet.create({});
---
+++ screens/result.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Result = () => {
+  return (
+    <View>
+      <Text>This is Result</Text>
+    </View>
+  );
+};
+
+export default Result;
+
+const styles = StyleSheet.create({});
